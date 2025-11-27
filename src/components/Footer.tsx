d72d45456fc55925173d6f6f1816328a98c7e2a5
Minseob Shin <minseob.shin11@gmail.com>
2025-11-26 22:53:42 -0600

    Fix Github href

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
               </svg>
             </a>
             <a
-              href="https://github.com/minseob-shin"
+              href="https://github.com/minseobshin11"
               target="_blank"
               rel="noopener noreferrer"
               className={`${mutedClass} ${hoverClass} transition-colors duration-300`}
